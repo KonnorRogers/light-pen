19b09c815f2a30c277d17dbdbd3f865c651859fb
konnorrogers <konnor5456@gmail.com>
2023-09-12 13:57:32 -0400

    working on base url

--- rollup.config.js
+++ rollup.config.js
@@ -26,7 +26,8 @@ export default [
       html({
         input: [
           './pages/**/*.html',
-        ]
+        ],
+        transformHtml: [html => html.replace('%BASE_URL%', process.env.BASE_URL)],
 
       }),
       nodeResolve(),
