91fc5e0e6d0be58de58d6378d115097693121cd8
Konnor Rogers <konnor5456@gmail.com>
2023-10-14 00:42:31 -0400

    fix custom elements manifest

--- custom-elements-manifest.config.js
+++ custom-elements-manifest.config.js
@@ -1,11 +1,8 @@
 // @ts-check
-import ts from 'typescript'
-import path from "path";
-import { expandTypesPlugin } from "cem-plugin-expanded-types"
 
 export default {
   /** Globs to analyze */
-  globs: ['exports/**/*.js', 'internal/**/*.js', 'types/**/*.d.ts'],
+  globs: ['./exports/**/*.js', './internal/**/*.js', './types/**/*.d.ts'],
   /** Globs to exclude */
   exclude: ['./node_modules', './docs'],
   /** Directory to output CEM to */
@@ -15,7 +12,7 @@ export default {
   /** Run in watch mode, runs on file changes */
   watch: process.argv.includes("--watch"),
   /** Include third party custom elements manifests */
-  dependencies: false,
+  dependencies: true,
   /** Output CEM path to `package.json`, defaults to true */
   packagejson: true,
   /** Enable special handling for litelement */
@@ -27,31 +24,5 @@ export default {
   /** Enable special handling for stencil */
   stencil: false,
   /** Provide custom plugins */
-  plugins: [
-    expandTypesPlugin()
-  ],
-
-  overrideModuleCreation: ({ts, globs}) => {
-    const configFile = ts.findConfigFile(
-      process.cwd(),
-      ts.sys.fileExists,
-      "tsconfig.json"
-    );
-
-    if (!configFile) throw "No tsconfig found."
-
-    const { config } = ts.readConfigFile(configFile, ts.sys.readFile);
-
-    const {options, errors} = ts.convertCompilerOptionsFromJson(config.compilerOptions, ".");
-
-    const program = ts.createProgram(globs, options);
-
-    const typeChecker = program.getTypeChecker()
-
-    return program
-      .getSourceFiles()
-      .filter((sf) => globs.find((glob) => {
-        return sf.fileName.includes(glob)
-      }))
-  },
+  plugins: [],
 }
