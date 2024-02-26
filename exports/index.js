2f33290ee030f8e8cf3f5db256e987e9c2f10ecb
konnorrogers <konnor5456@gmail.com>
2024-02-26 13:17:14 -0500

    add exports

--- exports/index.js
+++ exports/index.js
@@ -1,3 +1,5 @@
-import LightPen from "./light-pen-register.js";
-
-export default LightPen
+export { default as LightCode } from "./light-code-register.js";
+export { default as LightDisclosure } from "./light-disclosure-register.js";
+export { default as LightEditor } from "./light-editor-register.js";
+export { default as LightPen } from "./light-pen-register.js";
+export { default as LightPreview } from "./light-preview-register.js";
