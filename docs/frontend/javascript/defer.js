358ffabae37940bc02fe02ac0027994b94f3b95a
Konnor Rogers <konnor5456@gmail.com>
2024-05-26 00:27:10 -0400

    update bridgetown-ninja-keys

--- docs/frontend/javascript/defer.js
+++ docs/frontend/javascript/defer.js
@@ -56,18 +56,14 @@ import "../styles/defer.css"
       return this.staticData.concat(this.results)
     }
 
-    transformResult (result) {
-      let { id, title, categories, url, content, collection } = result
-      if (url.endsWith(".json")) {
-        return
-      }
-      return {
-        id,
-        title,
-        section: collection.name,
-        href: url,
-        content
-      }
+   transformResult (result) {
+      const action = super.transformResult(result)
+
+      if (!action) return
+
+      action.section = result.collection.name
+
+      return action
     }
 
     open () {
