7276061494906ffe110e807f8756ca0f82f0d6c5
Konnor Rogers <konnor5456@gmail.com>
2024-08-01 23:16:35 -0400

    remove light-diff-viewer

--- exports/components/light-diff-viewer/light-diff-viewer-globals.ts
+++
@@ -1,9 +0,0 @@
-import type LightDiffViewer from "./light-diff-viewer.js";
-
-declare global {
-  interface HTMLElementTagNameMap {
-    "light-diff-viewer": LightDiffViewer;
-  }
-}
-
-export {};
--- exports/index.js
+++ exports/index.js
@@ -1,5 +1,4 @@
 export { default as LightCode } from "./components/light-code/light-code-register.js";
-export { default as LightDiffViewer } from "./components/light-diff-viewer/light-diff-viewer-register.js";
 export { default as LightDisclosure } from "./components/light-disclosure/light-disclosure-register.js";
 export { default as LightEditor } from "./components/light-editor/light-editor-register.js";
 export { default as LightPen } from "./components/light-pen/light-pen-register.js";
