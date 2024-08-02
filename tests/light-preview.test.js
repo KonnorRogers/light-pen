bac34050b30107d8ecfed7ae6d665ae3b7646c8e
Konnor Rogers <konnor5456@gmail.com>
2024-08-01 23:27:13 -0400

    fix test suite

--- tests/light-preview.test.js
+++ tests/light-preview.test.js
@@ -1,1 +1,13 @@
-suit;
+import { html, fixture, assert } from "@open-wc/testing";
+import "../exports/components/light-preview/light-preview-register.js";
+
+suite("<light-preview>", () => {
+  test("Should render a component", async () => {
+    const el = await fixture(html` <light-preview></light-preview> `);
+
+    assert(
+      el.matches(":defined"),
+      `"${el.tagName.toLowerCase()}" element should be ":defined"`,
+    );
+  });
+});
